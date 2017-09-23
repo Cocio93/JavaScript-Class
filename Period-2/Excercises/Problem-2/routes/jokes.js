da2666a8f16f3568df940d3d646fe55c80271f89
Jonathan <jonathan.henriksen@yandex.com>
2017-09-23 20:35:36 +0200

    Problem-2 finished

--- Period-2/Excercises/Problem-2/routes/jokes.js
+++ Period-2/Excercises/Problem-2/routes/jokes.js
@@ -4,9 +4,23 @@ var path = require("path");
 var jokeModel = require('../model/jokes.js');
 
 router.get('/', function(req, res, next) {
-    randomJoke = jokeModel.jokes.getRandomJoke();
-    res.render('randomJoke', { randomJoke: randomJoke });
+    res.render('jokes', { jokes: jokeModel.jokes.allJokes });
   });
 
+router.get('/random', function(req, res, next) {
+    randomJoke = jokeModel.jokes.getRandomJoke();
+    res.render('randomJoke', { randomJoke: randomJoke });
+});
+
+router.get('/add', function(req, res, next) {
+    res.render('addJoke');
+});
+
+router.post('/addJoke', function(req, res, next) {
+    var joke = req.body;
+    console.log(joke);
+    jokeModel.jokes.addJoke(joke.joke);
+    res.render('addjoke', { jokeAdded: 'Joke successfully added'});
+});
 
 module.exports = router;
